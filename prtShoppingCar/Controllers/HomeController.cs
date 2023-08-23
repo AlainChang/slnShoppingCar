4c55e00126381da4014f9ff5262e5de89cf6decf
Alain Chang <alen7080891@gmail.ocm>
2023-08-23 23:13:49 +0800

    加入專案檔案。

---
+++ prtShoppingCar/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using prtShoppingCar.Models;
+
+namespace prtShoppingCar.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbShoppingCarEntities db = new dbShoppingCarEntities();
+        public ActionResult Index()
+        {
+            var product = db.tProduct
+                .OrderByDescending(m => m.fId).ToList();
+            return View(product);
+        }
+
+        //Get:Home/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //Post:Home/Login
+        [HttpPost]
+        public ActionResult Login(string fUseId, string fPwd)
+        {
+            var member = db.tMember
+                .Where(m=>m.fUseId==fUseId&&m.fPwd==fPwd)
+                .FirstOrDefault();
+            
+            if (member == null)
+            {
+                ViewBag.Message = "帳密錯誤，登入失敗";
+                return View();
+            }
+
+            //session
+            Session["WelCome"] = member.fName+ "歡迎光臨";
+            FormsAuthentication.RedirectFromLoginPage(fUseId, true);
+            return RedirectToAction("Index", "Member");
+        }
+
+        //Get:Home/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  ActionResult Register(tMember pMember)
+        {
+            if(ModelState.IsValid == false)
+            {
+                return View();
+            }
+
+            var member = db.tMember
+                .Where(m => m.fUseId == pMember.fUseId)
+                .FirstOrDefault();
+                
+            if(member==null)
+            {
+                db.tMember.Add(pMember);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            ViewBag.Message = "此帳號已有人使用，註冊失敗";
+            return View();
+        }
+    }
+}
---
+++ prtShoppingCar/Controllers/MemberController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using prtShoppingCar.Models;
+
+namespace prtShoppingCar.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+
+        dbShoppingCarEntities db = new dbShoppingCarEntities();
+        // GET: Member/Index
+        public ActionResult Index()
+        {
+            var products =
+                db.tProduct.OrderByDescending(m => m.fId).ToList();
+
+            return View("../Home/Index", "_LayoutMember",products);
+        }
+
+        //GET:Member/Logout
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Home");
+        }
+
+        //GET:Member/ShoppingCar
+        public ActionResult ShoppingCar()
+        {
+            string fUserId = User.Identity.Name;
+            var orderDetails = db.tOrderDetail.Where(m => m.fUserId == fUserId 
+            && m.fIsApproved == "否").ToList();
+            return View(orderDetails);
+        }
+
+        //GET:Member/AddCar
+        public ActionResult AddCar(string fPId)
+        {
+            string fUserId = User.Identity.Name;
+            var currentCar = db.tOrderDetail
+                .Where(m => m.fPId == fPId && m.fIsApproved == "否" && m.fUserId == fUserId).FirstOrDefault();
+
+            if(currentCar == null)
+            {
+                var product = db.tProduct
+                    .Where(m => m.fPId == fPId ).FirstOrDefault();
+                tOrderDetail orderDetail = new tOrderDetail();
+                orderDetail.fUserId = fUserId;
+                orderDetail.fPId = fPId;
+                orderDetail.fName = product.fName;
+                orderDetail.fPrice = product.fPrice;
+                orderDetail.fQty = 1;
+                orderDetail.fIsApproved = "否";
+                db.tOrderDetail.Add(orderDetail);
+            }
+            else
+            {
+                currentCar.fQty += 1;
+            }
+            db.SaveChanges();
+            return RedirectToAction("ShoppingCar");
+        }
+
+        //GET:Member/DeleteCar
+        public ActionResult DeleteCar(int fId)
+        {
+            var orderDetail = db.tOrderDetail.Where(m => m.fId == fId).FirstOrDefault();
+            db.tOrderDetail.Remove(orderDetail);
+            db.SaveChanges();
+            return RedirectToAction("ShoppingCar");
+        }
+
+        //POST:Member/shoppingCar
+        [HttpPost]
+        public ActionResult ShoppingCar(string fReceiver, string fEmail, string fAddress)
+        {
+            string fUseId = User.Identity.Name;
+            string guid = Guid.NewGuid().ToString();
+
+            tOrder order = new tOrder();
+            order.fOrderGuid = guid;
+            order.fUserId = fUseId;
+            order.fAddress = fAddress;
+            order.fReceiver = fReceiver;
+            order.fEmail = fEmail;
+            order.fDate = DateTime.Now;
+            db.tOrder.Add(order);
+
+            var carlist = db.tOrderDetail
+                .Where(m => m.fIsApproved == "否" && m.fUserId == fUseId).ToList();
+
+            foreach (var item in carlist)
+            {
+                item.fOrderGuid = guid;
+                item.fIsApproved = "是";
+            }
+            db.SaveChanges();
+            return RedirectToAction("OrderList");
+        }
+
+        //GET:Member/OrderList
+        public ActionResult OrderList()
+        {
+            string fUserId = User.Identity.Name;
+            var orders = db.tOrder.Where(m => m.fUserId == fUserId).OrderByDescending(m => m.fDate)
+                .ToList();
+            return View(orders);
+        }
+
+        //GET:Member/OrderDetail
+        public ActionResult OrderDetail(string fOrderGuid)
+        {
+            var orderDetails = db.tOrderDetail
+                .Where(m => m.fOrderGuid == fOrderGuid).ToList();
+            return View(orderDetails);
+        }
+    }
+}
